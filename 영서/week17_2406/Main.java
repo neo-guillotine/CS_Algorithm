ec612ac66c1d9af0ecea2d563750b7b2b123e924
youngseo.kim <youngseo.kim@saltlux.com>
2024-06-28 14:54:57 +0900

    240628

---
+++ 영서/week17_2406/Main.java
@@ -0,0 +1,17 @@
+package week17_2406;
+
+//public class BJ5212지구온난화_DOING {
+public class Main {
+    //updatedMap 새로 그리기. 한줄이 다 .이면 없애기
+    static int r,c;
+    static char map[][];
+
+    public static void main(String[] args) {
+        String t1="test";
+        String t2="test";
+        System.out.println(t1==t2);
+        System.out.println("test"==t1);
+        System.out.println(t1.equals(t2));
+        System.out.println("test".equals(t2));
+    }
+}
